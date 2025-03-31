738f427246787ef297350e186803aec02e899afa
daniperdomo <111296277+daniperdomo@users.noreply.github.com>
2025-03-31 13:07:30 -0400

    fix del fix lol

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -29,7 +29,7 @@ const Nav = () => {
     }, [])
 
     useEffect(() => {
-        fetch(`http://localhost:8081/api/productos`)
+        fetch(`/api/productos`)
             .then(response => response.json())
             .then(data => {
                 sessionStorage.setItem('searchResults', JSON.stringify(data))
@@ -38,7 +38,7 @@ const Nav = () => {
                 console.error('Error fetching products:', error)
             })
 
-        fetch(`http://localhost:8081/api/categoria`)
+        fetch(`/api/categoria`)
             .then(response => response.json())
             .then(data => {
                 setCategories(data)
